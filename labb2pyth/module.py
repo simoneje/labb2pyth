cfd845b3d3816bc4552f61dfa6df3073d837e480
simoneje <58808601+simoneje@users.noreply.github.com>
2020-01-28 21:32:43 +0100

    Add files via upload

--- labb2pyth/module.py
+++ labb2pyth/module.py
@@ -1,24 +1,12 @@
 import json
 
-def meny():
-    print("1.Läs in originalfil")
-    print("2.Visa json-data")
-    print("3.Lägg till person")
-    print("4.Ta bort person")
-    print("5.Spara fil")
-    print("6.exit")
-    val = ''
-    val = int(input(val))
-    if val == 1:
-        ReadFileMeny('personer.csv')
-    elif val == 2:
-        hej()
+#from json import JSONEncoder
+listan = []
+dicken = []
 
 
 
 def ReadFileMeny(filename):
-    listan = []
-    dicken = []
     filen = filename
     try:
         with open(filen, encoding="utf-8") as f_obj:
@@ -28,15 +16,73 @@ def ReadFileMeny(filename):
         print(FileNotFoundError)   
     for y in listan:
         info = y.rstrip('\n').split(";")
-        dicken.append({"name": info[0], "efternamn": info[1], "användarnamn": info[2], "email": info[3]})
+        dicken.append({"name": info[0],"efternamn": info[1],"användarnamn": info[2],"email": info[3]})
     for a in dicken:
         print(a)
+    return dicken
+        
+    # print("Vill du spara denna dict till en json-fil? Y/N")
+    # val = ''
+    # val = str(input(val))
+    # if val == 'y':
+    #     SaveDictTillJson(dicken)
+    
+    
+
+def SaveDictTillJson(list):
+    listan = list
+    try:
+        with open('./hej/personer.json', 'w', encoding="utf-8") as json_dicken:
+            json.dump(listan, json_dicken,ensure_ascii=False, indent=4)
+    except FileNotFoundError:
+        print(FileNotFoundError)
+
+def VisaJsonData(filename):
+    try:
+        with open(filename, "r", encoding="utf-8") as sJson:
+            return json.load(sJson)
+    except FileNotFoundError as error:
+        print(error)
+
+def SparaHuman(filename):
+    try:
+        with open(filename, "w", encoding="utf-8") as sHuman:
+            json.dump(dicken, sHuman, ensure_ascii=False, indent=4)
+    except FileNotFoundError as error:
+        print(error)
+
+def CreateHuman():
+    namn = input("Skriv in nytt namn: ")
+    enamn = input("Skriv in efternamn: ")
+    user = input("Skriv in nytt användarnamn: ")
+    mejl = input("Skriv in nytt mejl: ")
+    dicken.append({
+        "name": namn,
+        "efternamn": enamn,
+        "användarnamn": user,
+        "email": mejl
+    })
+
+def RemoveHuman():
+    human = input("Skriv in efternamn på den du vill ta bort: ")
+    for i in range(len(dicken)):
+        if human == dicken[i]["efternamn"]:
+            del dicken[i]
+            break
 
 
-def SaveDictTillJson():
-    print("yo")
-
-def hej():
-    print("hej")
-
-meny()
+# def SaveObjectTillJson(object):
+#     personen = object
+#     with open('./hej/personer.json', 'w') as json_person:
+#         json.append(personen, json_person)
+  
+# class skapaPerson():
+#     def __init__(self, name, lastname, username, email):
+#         self.name = name
+#         self.lastname = lastname
+#         self.username = username
+#         self.email = email
+#     def presenteraName(self):
+#         return self.name + ' ' + self.lastname + ' ' + self.username + ' ' + self.email
+#     #def info(self):
+#     #    print(f'din person heter{self.name.title}, har efternamn {self.lastname}')
---
+++ labb2pyth/program.py
@@ -0,0 +1,34 @@
+import module
+
+def meny():
+    print("1.Läs in originalfil")
+    print("2.Visa json data")
+    print("3.Lägg till person")
+    print("4.Ta bort person")
+    print("5.Spara fil")
+    print("6.exit")
+    val = ''
+    val = int(input(val),10)
+    if val == 1:
+        module.ReadFileMeny('./hej/personer.csv')
+        BackToMeny()
+    elif val == 2:
+        module.visa = module.VisaJsonData('personer.json')
+        print(module.visa)
+        BackToMeny()        
+    elif val == 3:
+        module.CreateHuman()
+        BackToMeny()
+    elif val == 4:
+        module.RemoveHuman()
+        BackToMeny()
+    elif val == 5:
+        module.SparaHuman('personer.json')
+        BackToMeny()
+
+def BackToMeny():
+    print("VILL DU GÅ TILLBAKA TILL MENYN?! Y/N")
+    val = input()
+    if val == 'Y' or val == 'y' or val == 'yes' or val == 'Yes':
+        meny()
+meny()
